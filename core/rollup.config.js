bc905dab2ab9c6421f5d99da95cb06fc3844c0a4
Mads Felskov Agersten <mads@felskov-aps.dk>
2025-02-20 14:44:43 +0100

    feat: implement multi-export from within @direct.dev/client (#60)
    * refactor: prepare multiple exports from client package
    
    * chore: rename @direct.dev/core --> @direct.dev/client
    
    * chore: fix package-lock.json

--- core/rollup.config.js
+++ core/rollup.config.js
@@ -1,3 +1,12 @@
+import path from "path";
+
 import withRecommendedConfig from "@direct.dev/tooling-rollup-config/recommended";
 
-export default withRecommendedConfig(import.meta.dirname, {});
+export default withRecommendedConfig(import.meta.dirname, {
+  input: {
+    index: path.resolve(import.meta.dirname, "src", "index.ts"),
+    "index.ethers": path.resolve(import.meta.dirname, "src", "index.ethers.ts"),
+    "index.wagmi": path.resolve(import.meta.dirname, "src", "index.wagmi.ts"),
+    "index.web3": path.resolve(import.meta.dirname, "src", "index.web3.ts"),
+  },
+});

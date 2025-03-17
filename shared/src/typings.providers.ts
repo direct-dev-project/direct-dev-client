7161c04b71d3ec8fc36fc4ff59a48a7735581f04
Mads Felskov Agersten <mads@felskov-aps.dk>
2025-03-17 13:50:09 +0100

    feat: add configurations for all initially supported networks and providers (#92)

--- shared/src/typings.providers.ts
+++ shared/src/typings.providers.ts
@@ -2,10 +2,10 @@
  * Definition of supported Web3 Data providers within the Direct.dev
  * infrastructure.
  */
-export type SupportedProviderId = "ankr" | "alchemy" | "quicknode";
+export type SupportedProviderId = "ankr" | "alchemy" | "drpc" | "infura" | "quicknode";
 
 /**
- * Definition of supported Web3 chain types, used to determine how to correctly
+ * Definition of supported Web3 chain types|  used to determine how to correctly
  * handle analysis of incoming requests and responses.
  */
 export type SupportedChainType = "EVM" | "SVM";
@@ -13,10 +13,35 @@ export type SupportedChainType = "EVM" | "SVM";
 /**
  * Definition of all supported chains by Direct.dev.
  */
-export type SupportedChainId = "ethereum" | "solana";
+export type SupportedChainId =
+  | "base"
+  | "arbitrum"
+  | "optimism"
+  | "sonic"
+  | "bsc"
+  | "avalanche"
+  | "ethereum"
+  | "polygon";
 
 /**
  * Definition of networks supported by Direct.dev (internally maps to specific
  * provider nodes)
  */
-export type SupportedNetworkId = "ethereum-mainnet" | "ethereum-testnet" | "solana-mainnet";
+export type SupportedNetworkId =
+  | "base-sepolia"
+  | "base"
+  | "arbitrum"
+  | "arbitrum-sepolia"
+  | "optimism"
+  | "optimism-sepolia"
+  | "sonic-testnet"
+  | "sonic"
+  | "bsc"
+  | "bsc-testnet"
+  | "avalanche"
+  | "avalanche-fuji"
+  | "ethereum"
+  | "ethereum-sepolia"
+  | "ethereum-holesky"
+  | "polygon"
+  | "polygon-amoy";

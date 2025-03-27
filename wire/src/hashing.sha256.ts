0a54bb446922f03986cab731df422b0b87e0b1cd
Mads Felskov Agersten <mads@felskov-aps.dk>
2025-03-27 14:49:32 +0100

    ci: prepare release of client libraries (#104)

--- wire/src/hashing.sha256.ts
+++ wire/src/hashing.sha256.ts
@@ -1,8 +1,24 @@
+let hasWarnedAboutInsecureContext = false;
+
 /**
  * Web APIs implementation to generate a SHA-256 hash in hexadecimal output
  * from the given input string
  */
 export async function sha256(input: string): Promise<string> {
+  if (crypto.subtle === undefined) {
+    if (!hasWarnedAboutInsecureContext) {
+      // eslint-disable-next-line no-console
+      console.warn(
+        "Direct.dev: You're running in an insecure context, which prevents using crypto.subtle for sha256 hashing. Your application should work just fine, but predictive prefetching will fail to deliver content ahead of time.",
+      );
+    }
+
+    hasWarnedAboutInsecureContext = true;
+
+    // cannot hash in insecure contexts, simply use input in it's raw form...
+    return input;
+  }
+
   // Encode the input string into a Uint8Array using TextEncoder.
   const encoder = new TextEncoder();
   const data = encoder.encode(input);

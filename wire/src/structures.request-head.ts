e516b3099d07da4d8ecedb5a209a24f5c5558b64
Mads Felskov Agersten <mads@felskov-aps.dk>
2025-05-09 21:39:24 +0200

    feat: add support for request-level locking of block height

--- wire/src/structures.request-head.ts
+++ wire/src/structures.request-head.ts
@@ -3,6 +3,7 @@ import { Wire } from "./core.wire.js";
 
 export type RequestHead = {
   sessionId: string;
+  blockHeight?: string;
 };
 
 /**
@@ -10,15 +11,17 @@ export type RequestHead = {
  * samples collected in the client layer.
  */
 export const requestHead = new Wire<RequestHead>({
-  encode: (input) => pack.str(input.sessionId),
+  encode: (input) => pack.str(input.sessionId) + pack.nullableStr(input.blockHeight),
   decode: (input, cursor) => {
     const sessionId = unpack.str(input, cursor);
+    const blockHeight = unpack.nullableStr(input, sessionId[1]);
 
     return [
       {
         sessionId: sessionId[0],
+        blockHeight: blockHeight[0] ?? undefined,
       },
-      sessionId[1],
+      blockHeight[1],
     ];
   },
 });
